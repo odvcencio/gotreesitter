753c2513cd3b74921c989cda312a8671ce10f0fb
Oscar Villavicencio <9220505+odvcencio@users.noreply.github.com>
2026-09-20 07:06:03 -0700

    update(cgo_harness): Refresh A0 manifest and grammar lock checksums
    - Update dispatcher_census_a0_manifest_v1.json grammar_lock_sha256 to match the rebuilt fixture
    - Sync A0 manifest SHA constants in julia, perl, solidity, templ, wgsl, wolfram receipt tests
    
    Buckley-Change-Hash: sha256:d58943634c04042db03b671994b76fa583109985e7c074cc0b4515f5366e7a26
    Buckley-Change-Stats: files=7 insertions=7 deletions=7 binaries=0

--- cgo_harness/solidity_next_live_probe_test.go
+++ cgo_harness/solidity_next_live_probe_test.go
@@ -50,7 +50,7 @@ const (
 	solidityNextGrammarRepo           = "https://github.com/JoranHonig/tree-sitter-solidity"
 	solidityNextGrammarCommit         = "048fe686cb1fde267243739b8bdbec8fc3a55272"
 	solidityNextCArtifactSHA256       = "5bafc32251964c20e5a61f74ec32d001fcc5776e7ed3b7ed8621fd7fd96d6a2a"
-	solidityNextA0ManifestSHA256      = "45711c4a05ec4c183af16bd143d16f98506bb05905ea9e359436ea8f7bed2833"
+	solidityNextA0ManifestSHA256      = "ed2395a0286229b4bb63afe2a87aa0fc2591398afcc2e31602736b8a7eaf3d95"
 	solidityNextTrackedManifestSHA256 = "881462ec0eb65dd6643eab13a9727743efef4578749c30c63dfab1a15ef821b0"
 	solidityNextCorpusLockSHA256      = "41c744279c8b1d7c9fe7b1b8e26fba733423e77cd48efea46927309c22d163ea"
 )
--- cgo_harness/templ_n31q_dispatcher_blocker_receipt_test.go
+++ cgo_harness/templ_n31q_dispatcher_blocker_receipt_test.go
@@ -20,7 +20,7 @@ const (
 	templN31qGrammarRepo            = "https://github.com/vrischmann/tree-sitter-templ"
 	templN31qGrammarCommit          = "1c6db04effbcd7773c826bded9783cbc3061bd55"
 	templN31qBlobSHA256             = "78f20ce45f9a4df12c458aadfbe9a98c80572bb13e0e2d01ffc43060e8d04701"
-	templN31qA0ManifestSHA256       = "45711c4a05ec4c183af16bd143d16f98506bb05905ea9e359436ea8f7bed2833"
+	templN31qA0ManifestSHA256       = "ed2395a0286229b4bb63afe2a87aa0fc2591398afcc2e31602736b8a7eaf3d95"
 	templN31qCorpusSidecarSHA256    = "2b2209597d1701ccc813bd35d1685b5b13730e6ebd285e66485ce812e35877cf"
 	templN31qCorpusLockSHA256       = "41c744279c8b1d7c9fe7b1b8e26fba733423e77cd48efea46927309c22d163ea"
 	templN31qCArtifactSHA256        = "91e455a6392a736912a481f0322c67bf571896c067ad0c7fba4ce4e9a7038081"
